83459429d3d558656f24e6fc65422e4bd968538a
MrFox131 <enginer385@gmail.com>
2020-01-14 01:10:31 +1100

    Started index.php and renamed playlists for proper ordering

--- course/index.php
+++ course/index.php
@@ -1,3 +1,13 @@
-<?
-
+<html>
+<?php
+$dir = "/mnt/c/users/Fox and Dragon/Desktop/foxford_courses/course/playlists";
+$files = scandir($dir);
+foreach ($files as $filename){
+	if ($filename=="." or $filename==".."){
+		continue;
+	}
+	print($filename);
+	print("<br>");
+}
 ?>
+</html>
