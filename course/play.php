69a02c9b2950e1f8bff503caf97b8e8ed94d62da
MrFox131 <enginer385@gmail.com>
2020-01-14 18:20:54 +1100

    Safe version of video selection

--- course/play.php
+++ course/play.php
@@ -5,7 +5,15 @@
 <div id="player"></div>
 <script src="playerjs.js"></script>
 <script>
-   var player = new Playerjs({id:"player", file:"playlists/<?php echo htmlspecialchars($_GET["playlist"])?>"});
+   var player = new Playerjs({id:"player", file:"playlists/<?php
+	$dir="./playlists";
+	$files=scandir($dir);
+	if (in_array($_GET["playlist"], $files)){
+		echo htmlspecialchars($_GET["playlist"]);
+	} else {
+		echo $files[2];
+	}
+?>"});
 </script>
 
 </html>
